b5c1d9cace33bb72c509ba08a7c5bd25785c1f8a
GOssipov <64108017+GOssipov@users.noreply.github.com>
2024-05-09 19:46:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ app=FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message":"Hello World from Smurfcoders"}
+    return {"message":"Landing Page"}
 
 @app.get("/test")
 async def root():
-    return {"message":"this is test"}
+    return {"message":"Colleges Page"}
     
