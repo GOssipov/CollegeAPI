c4418d2f12dcc25a39d21fb0e10e2aa968c3d762
GOssipov <64108017+GOssipov@users.noreply.github.com>
2024-05-09 20:52:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
-app=FastAPI()
+
+app = FastAPI()
+
+# Sample list of college names
+college_names = ["College A", "College B", "College C"]
 
 @app.get("/")
 async def root():
-    return {"message":"Landing Page"}
+    return {"message": "Landing Page"}
 
-@app.get("/test")
-async def root():
-    return {"message":"Colleges Page"}
-    
+@app.get("/current_college")
+async def current_college():
+    return college_names
+
+@app.get("/chosen_college")
+async def chosen_college():
+    return college_names
